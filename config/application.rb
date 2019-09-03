07079b9522ef6341cd2b816557d454cbd1dc68e9
kaseken <k.kaseda0925@gmail.com>
2019-09-03 23:30:03 +0900

    configuration for amazon api

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'amazon/ecs'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,12 +9,12 @@ Bundler.require(*Rails.groups)
 
 module Booklabo
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    Amazon::Ecs.options = {
+      associate_tag:     Rails.application.credentials.amazon_api[:associate_tag],
+      AWS_access_key_id: Rails.application.credentials.amazon_api[:access_key],
+      AWS_secret_key:    Rails.application.credentials.amazon_api[:secret_key]
+    }
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,3 @@
 require_relative 'application'
 
 Rails.application.initialize!
-
-Amazon::Ecs.options = {
-  associate_tag:     Rails.application.credentials.amazon_api[:associate_tag],
-  AWS_access_key_id: Rails.application.credentials.amazon_api[:access_key],
-  AWS_secret_key:    Rails.application.credentials.amazon_api[:secret_key]
-}
