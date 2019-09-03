27bd4267a80bea7efafa91a8ac87993cd80be6b2
kaseken <k.kaseda0925@gmail.com>
2019-09-03 19:55:00 +0900

    add gem amazon-ecs

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
-# Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
 Rails.application.initialize!
+
+Amazon::Ecs.options = {
+  associate_tag: Rails.application.credentials.amazon_api[:associate_tag],
+  AWS_access_key_id: application.credentials.amazon_api[:access_key],
+  AWS_secret_key: Rails.application.credentials.amazon_api[:secret_key]
+}
