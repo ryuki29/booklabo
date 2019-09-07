f8212cbaf09a5b5206a5c029fb9a95a6ec4e6f58
kaseken <k.kaseda0925@gmail.com>
2019-09-07 20:48:05 +0900

    create modal for posting reviews

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -31,21 +31,85 @@ $(document).on("turbolinks:load", function() {
           .css("visibility", "hidden");
       }
     );
-
-    $(".add-book").on("click", function() {
-      let title = $(this).data("title");
-      let author = $(this).data("author");
-      let image = $(this).data("image");
-      let uid = $(this).data("uid");
-
-      $("#modal-book-title").text(title);
-      $("#modal-book-author").text(author);
-      $("#modal-book-img").attr("src", image);
-
-      $("#modal-book-data").attr("data-title", title);
-      $("#modal-book-data").attr("data-author", author);
-      $("#modal-book-data").attr("data-image", image);
-      $("#modal-book-data").attr("data-uid", uid);
-    });
   }
+
+  $(".add-book").on("click", function() {
+    let title = $(this).data("title");
+    let authors = $(this).data("authors");
+    let image = $(this).data("image");
+    let uid = $(this).data("uid");
+
+    $("#modal-book-title").text(title);
+    $("#modal-book-author").text(author);
+    $("#modal-book-img").attr("src", image);
+
+    $("#readBook").attr("data-title", title);
+    $("#readBook").attr("data-authors", authors);
+    $("#readBook").attr("data-image", image);
+    $("#readBook").attr("data-uid", uid);
+  });
+
+  $(".fa-star").hover(
+    function() {
+      $(this).addClass("star-hover");
+      $(this)
+        .prevAll()
+        .addClass("star-hover");
+    },
+    function() {
+      $(this).removeClass("star-hover");
+      $(this)
+        .prevAll()
+        .removeClass("star-hover");
+    }
+  );
+
+  $(".fa-star").on("click", function() {
+    $(".fa-star").removeClass("star-active");
+    $(this).addClass("star-active");
+    $(this)
+      .prevAll()
+      .addClass("star-active");
+    let rating = $(this).data("rating");
+    $("#book-rating").attr("value", rating);
+  });
+
+  $("#rating-reset").on("click", function() {
+    $("#book-rating").attr("value", 0);
+    $(".fa-star").removeClass("star-active");
+  });
+
+  $("#date-check").on("click", function() {
+    if ($(this).prop("checked") === true) {
+      $("#date-input")
+        .attr("disabled", true)
+        .addClass("form-disabled");
+    } else {
+      $("#date-input")
+        .attr("disabled", false)
+        .removeClass("form-disabled");
+    }
+  });
+
+  $("#review-text").on("keyup", function() {
+    let wordLength = $(this).val().length;
+    $("#word-count").text(wordLength);
+
+    if (wordLength > 255) {
+      $("#word-count").css("color", "#d65656");
+      $("#review-submit").attr("disabled", true);
+    } else {
+      $("#word-count").css("color", "#1ea1f1");
+      $("#review-submit").removeAttr("disabled");
+    }
+  });
+
+  $("#review-submit").hover(
+    function() {
+      $(this).css("opacity", 0.8);
+    },
+    function() {
+      $(this).css("opacity", 1);
+    }
+  );
 });
