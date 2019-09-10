c1c98e39c3ff3096edde0f5e7e5546f71bc0ddc4
kaseken <k.kaseda0925@gmail.com>
2019-09-10 19:26:38 +0900

    update create_books.js (enable to post review in production)

--- app/assets/javascripts/create_books.js
+++ app/assets/javascripts/create_books.js
@@ -58,7 +58,7 @@ $(document).on("turbolinks:load", function() {
       dataType: "json"
     })
       .done(function(data) {
-        let url = "/users/" + data.user_id;
+        let url = "/users/" + data.user_id + "?status=" + data.book_status;
         window.location.replace(url);
       })
       .fail(function() {
