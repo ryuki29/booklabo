e60cbeb343d2bdc0c70c8521002d041b6334d694
kaseken <k.kaseda0925@gmail.com>
2019-09-12 21:32:32 +0900

    update users.rb for omniauth-twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,25 +8,29 @@ class User < ApplicationRecord
   has_many :user_books, dependent: :destroy
   has_many :books, through: :user_books
 
-  # def self.find_for_oauth(auth)
-  #   user = User.where(uid: auth.uid, provider: auth.provider).first
+  def self.find_for_oauth(auth)
+    sns_uid = SnsUid.where(uid: auth.uid, provider: auth.provider).first
 
-  #   unless user
-  #     user = User.create(
-  #       uid:      auth.uid,
-  #       provider: auth.provider,
-  #       email:    User.dummy_email(auth),
-  #       username: auth.info.nickname,
-  #       password: Devise.friendly_token[0, 20]
-  #     )
-  #   end
+    unless sns_uid
+      user = User.create(
+        email: User.dummy_email(auth),
+        name: auth.info.nickname,
+        password: Devise.friendly_token[0, 20]
+      )
 
-  #   user
-  # end
+      sns_uid = SnsUid.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        user: user
+      )
+    end
 
-  # private
+    return user
+  end
 
-  # def self.dummy_email(auth)
-  #   "#{auth.uid}-#{auth.provider}@example.com"
-  # end
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
