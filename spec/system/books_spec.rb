4d5a403e6fba2261f41b1d56a0368ad53734c6e3
kaseken <k.kaseda0925@gmail.com>
2019-09-22 16:55:13 +0900

    add integration test for creating read-book and review

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -47,45 +47,53 @@ describe 'Books', type: :system do
              "authors"=>["夏目漱石"],
              "imageLinks"=>{"thumbnail"=>"http://books.google.com/books/content?id=MXJT-12RchcC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"}}}]}
       allow(Book).to receive(:search_books).and_return(json)
+
+      sign_in(user)
+      visit root_path
+      fill_in 'keyword', with: '夏目漱石'
+      click_button 'search-submit'
+      expect(page).to_not have_content "本を登録する"
+      find('.add-book-cover').hover
+      expect(page).to have_content "本を登録する"
+      find(".add-book").click
     end
 
     it "読んだ本を登録できる" do
+      expect do
+        find("#read-book").click
+        expect(page).to have_content "読んだ本に登録する"
+        fill_in "review-text", with: "レビュー機能の統合テスト"
+        click_button "review-submit"
+
+        expect(page).to have_selector ".user-show"
+        expect(page).to have_content user.name
+        expect(page).to have_css('#read.active')
+        expect(page).to have_content "坊っちゃん"
+      end.to  change(Book, :count).by(1)
+         .and change(Review, :count).by(1)
+         .and change(UserBook, :count).by(1)
     end
 
     it "読みたい本を登録できる" do
-      sign_in(user)
       expect do
-        visit root_path
-        fill_in 'keyword', with: '夏目漱石'
-        click_button 'search-submit'
-        expect(page).to_not have_content "本を登録する"
-        find('.add-book-cover').hover
-        expect(page).to have_content "本を登録する"
-        find(".add-book").click
         find("#will-read-book").click
-
         expect(page).to have_selector ".user-show"
         expect(page).to have_content user.name
-        expect(page).to have_content "プロフィールを編集"
-      end.to change(Book, :count).by(1)
+        expect(page).to have_css('#will-read.active')
+        expect(page).to have_content "坊っちゃん"
+      end.to  change(Book, :count).by(1)
+         .and change(UserBook, :count).by(1)
     end
 
     it "読んでる本を登録できる" do
-      sign_in(user)
       expect do
-        visit root_path
-        fill_in 'keyword', with: '夏目漱石'
-        click_button 'search-submit'
-        expect(page).to_not have_content "本を登録する"
-        find('.add-book-cover').hover
-        expect(page).to have_content "本を登録する"
-        find(".add-book").click
         find("#reading-book").click
-
         expect(page).to have_selector ".user-show"
         expect(page).to have_content user.name
-        expect(page).to have_content "プロフィールを編集"
-      end.to change(Book, :count).by(1)
+        expect(page).to have_css('#reading.active')
+        expect(page).to have_content "坊っちゃん"
+      end.to  change(Book, :count).by(1)
+         .and change(UserBook, :count).by(1)
     end
   end
 end
