3a00575031ced00c934b38723e61334521d0e5ce
kaseken <k.kaseda0925@gmail.com>
2019-09-23 13:25:23 +0900

    add simplecov to check test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter do |source_file|
+    filters.clear
+    source_file.lines.count < 5
+  end
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -6,7 +14,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'capybara/rspec'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
